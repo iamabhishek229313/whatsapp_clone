30bd17593d524e6f41b00d680e961ac08be95909
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-03-15 20:09:02 +0530

    ListView is now upto the mark .

--- lib/Screens/ChatScreen.dart
+++ lib/Screens/ChatScreen.dart
@@ -10,51 +10,77 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: dummyMsg.length,
-        itemBuilder: (context, index) {
-          return new ListTile(
-            title: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(
-                  dummyMsg[index].name,
-                  style: new TextStyle(
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                new SizedBox(height : ScreenUtil().setHeight(8.0)),
-              ],
-            ),
-            subtitle: new Container(
-              height: ScreenUtil().setHeight(65.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Text(
-                    dummyMsg[index].message,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: new TextStyle(
-                      fontWeight: FontWeight.w400
+    return Padding(
+      padding: EdgeInsets.only(top:ScreenUtil().setHeight(20.0)),
+      child: ListView.builder(
+          itemCount: dummyMsg.length,
+          itemBuilder: (context, index) {
+            return new Container(
+                padding:
+                    EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(20.0)),
+                height: ScreenUtil().setHeight(120.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new ClipRRect(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      child: new Image(
+                        fit: BoxFit.cover,
+                        image: new AssetImage(dummyMsg[index].imageURL),
+                        width: ScreenUtil().setWidth(104.0),
+                        height: ScreenUtil().setHeight(90.0),
+                      ),
                     ),
-                  ),
-                  new SizedBox(height : ScreenUtil().setHeight(10.0)),
-                  new Divider()
-                ],
-              ),
-            ),
-            leading: new ClipRRect(
-              borderRadius: new BorderRadius.circular(50.0),
-              child: new Image(
-                  fit: BoxFit.cover,
-                  width: ScreenUtil().setWidth(100.0),
-                  height: ScreenUtil().setHeight(100.0),
-                  image: new AssetImage(dummyMsg[index].imageURL)
-                ),
-            ),
-          );
-        });
+                    new SizedBox(
+                      width: ScreenUtil().setWidth(30.0),
+                    ),
+                    new Expanded(
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              new Container(
+                                width: ScreenUtil().setWidth(400.0),
+                                child: new Text(
+                                  dummyMsg[index].name,
+                                  style: new TextStyle(
+                                      fontSize: ScreenUtil().setSp(32.0),
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              new Text(
+                                dummyMsg[index].time,
+                                style: new TextStyle(
+                                    fontSize: ScreenUtil().setSp(20.0),
+                                    fontWeight: FontWeight.w400),
+                              )
+                            ],
+                          ),
+                          new SizedBox(height: ScreenUtil().setHeight(8.0)),
+                          new Container(
+                            width: ScreenUtil().setWidth(500.0),
+                            child: new Text(
+                              dummyMsg[index].message,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: ScreenUtil().setSp(25.0)),
+                            ),
+                          ),
+                          new SizedBox(height: ScreenUtil().setHeight(15.0)),
+                          new Container(
+                              width: ScreenUtil().setWidth(550.0),
+                              child: new Divider())
+                        ],
+                      ),
+                    )
+                  ],
+                ));
+          }),
+    );
   }
 }
