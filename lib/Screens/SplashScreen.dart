2cd1cde13509d61f1b4780904956c76dc1444f9a
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-03-18 17:25:32 +0530

    SplashScreen is Added .

---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:whatsapp_design/Screens/LogInPage.dart';
+
+class SplashScrren extends StatefulWidget {
+  @override
+  _SplashScrrenState createState() => _SplashScrrenState();
+}
+
+class _SplashScrrenState extends State<SplashScrren> {
+
+  @override
+  void initState() { 
+    super.initState();
+    Timer(Duration(
+      seconds: 1,
+    ), () {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => LoginPage())) ;
+    }
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Theme.of(context).cardColor,
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            child: new Image(
+                image: new AssetImage('assets/images/WhatsApp_Logo_4.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_design/HomePage.dart';
+import 'package:whatsapp_design/Screens/SplashScreen.dart';
 
 List<CameraDescription> camera ;
 
@@ -20,10 +21,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor : Color.fromRGBO(7, 94, 84, 1.0),
         secondaryHeaderColor: Color.fromRGBO(37, 211, 102,1.0),
-        highlightColor: Color.fromRGBO(18, 140, 126, 1.0)
-        
+        highlightColor: Color.fromRGBO(18, 140, 126, 1.0),
+        cardColor: Color.fromRGBO(250, 250, 250, 1.0)
       ),
-      home: HomePage(),
+      home: SplashScrren(),
     );
   }
 }
